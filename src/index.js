d2e93c5b976ba4381b6d47afc5ade8cdeb37fa54
phill <freecorleone988@gmail.com>
2018-07-26 17:31:34 +0800

    do

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react'
 import './styles.scss'
 
 export default class extends Component {
+
+  
   render() {
     return <div className="rc-address-selector">
       <h2 className="onepx-border">行政区划选择</h2>
       <dl>
         <dt>
           <ul className="rc-ds-address onepx-border">
-            <li className="active">四川省</li>
+            <li className={'active' ? }>四川省</li>
             <li>成都市</li>
             <li>武侯区</li>
           </ul>
