1452520d6d0c50c9f7c90bb9b9b3a9766374f033
phill <freecorleone988@gmail.com>
2018-07-27 09:20:55 +0800

    styles and switch

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-import React, {Component} from 'react'
-import './styles.scss'
+import React, { Component } from "react";
+import "./styles.scss";
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 0,
+      address: [{ name: "四川" }, { name: "成都市" }, { name: "武侯区" }]
+    };
+  }
 
-  
   render() {
-    return <div className="rc-address-selector">
-      <h2 className="onepx-border">行政区划选择</h2>
-      <dl>
-        <dt>
-          <ul className="rc-ds-address onepx-border">
-            <li className={'active' ? }>四川省</li>
-            <li>成都市</li>
-            <li>武侯区</li>
-          </ul>
-        </dt>
-        <dd>
-          123
-        </dd>
-      </dl>
-    </div>
+    let state = this.state;
+    return (
+      <div className="rc-address-selector">
+        <h2 className="onepx-border">行政区划选择</h2>
+        <dl>
+          <dt>
+            <ul className="rc-ds-address onepx-border">
+              {state.address.map((v, i) => (
+                <li
+                  className={i === state.active ? "active" : ""}
+                  key={i}
+                  onClick={() => {
+                    this.setState({ active: i });
+                  }}
+                >
+                  {v.name}
+                </li>
+              ))}
+            </ul>
+          </dt>
+          <dd>123</dd>
+        </dl>
+      </div>
+    );
   }
 }
