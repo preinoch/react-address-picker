80acf526bc59708944a7f6e3c22017cd42f140af
phill <freecorleone988@gmail.com>
2018-07-27 11:22:06 +0800

    set animation

--- src/index.js
+++ src/index.js
@@ -6,30 +6,43 @@ export default class extends Component {
     super(props);
     this.state = {
       active: 0,
+      activeStyle: {},
       address: [{ name: "四川" }, { name: "成都市" }, { name: "武侯区" }]
     };
   }
 
+  changeIndex(i) {
+    const target = this.refs.rcAddress.childNodes[i];
+    const left = target.offsetLeft + "px";
+    const right =
+      screen.width - target.offsetLeft - target.offsetWidth + "px";
+    this.setState({ activeStyle: { left, right } });
+  }
+
+  componentDidMount() {
+    this.changeIndex(0)
+  }
+
   render() {
     let state = this.state;
+    
     return (
       <div className="rc-address-selector">
         <h2 className="onepx-border">行政区划选择</h2>
         <dl>
           <dt>
-            <ul className="rc-ds-address onepx-border">
+            <ul ref="rcAddress" className="rc-ds-address onepx-border">
               {state.address.map((v, i) => (
                 <li
                   className={i === state.active ? "active" : ""}
                   key={i}
-                  onClick={() => {
-                    this.setState({ active: i });
-                  }}
+                  onClick={this.changeIndex.bind(this, i)}
                 >
                   {v.name}
                 </li>
               ))}
             </ul>
+            <div className="rc-ds-active" style={state.activeStyle} />
           </dt>
           <dd>123</dd>
         </dl>
